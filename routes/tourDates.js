45f00b8ed66d8739e2a81a362426f3cc7d983705
Phil Todisco <philtodiscoglass@gmail.com>
2023-05-01 19:27:57 -0400

    Working version

--- routes/tourDates.js
+++ routes/tourDates.js
@@ -1,60 +1,34 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
 const TourDate = require('../models/tourDate')
+const apiKey = process.env.API_KEY
 
-const API_KEY = process.env.API_KEY;
-const HEROKU_APP_URL = 'https://dttc-api.herokuapp.com';
-
-// Authentication middleware
-function authMiddleware(req, res, next) {
-  const apiKey = req.headers['x-api-key'];
-  if (!apiKey || apiKey !== API_KEY) {
-    res.status(401).json({ error: 'Unauthorized' });
-  } else {
+router.use((req, res, next) => {
+    req.headers['x-api-key'] = apiKey;
     next();
+  });
+
+function checkApiKey(req, res, next) {
+    const apiKeyHeader = req.headers['x-api-key'];
+    if (apiKeyHeader === apiKey) {
+      next();
+    } else {
+      res.status(401).send('Unauthorized');
+    }
   }
-}
 
-// Proxy route
-router.get('/', authMiddleware, async (req, res) => {
-  try {
-    const response = await axios.get(`${HEROKU_APP_URL}/api/tourDates`);
-    const tourDates = response.data;
-    res.json(tourDates);
-  } catch (err) {
-    console.error('Error in tour dates request:', err);
-    res.status(500).send('Server error');
-  }
-});
-
-// router.use((req, res, next) => {
-//     req.headers['x-api-key'] = apiKey;
-//     next();
-//   });
-
-// function checkApiKey(req, res, next) {
-//     const apiKeyHeader = req.headers['x-api-key'];
-//     if (apiKeyHeader === apiKey) {
-//       next();
-//     } else {
-//       res.status(401).send('Unauthorized');
-//     }
-//   }
-  
-// router.get('/', checkApiKey, async (req, res) => {
-//     try {
-//         const tourDates = await TourDate.find()
-//         res.json(tourDates)
-//     } catch (err) {
-//         res.status(500).json({ message: err.message })
-//     }
-// })
+router.get('/', checkApiKey, async (req, res) => {
+    try {
+        const tourDates = await TourDate.find()
+        res.json(tourDates)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
-
 router.post('/', async (req, res) => {
     const tourDate = new TourDate({
         date: req.body.date,
@@ -72,13 +46,10 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message})
     }
 })
-
 router.patch('/:id', (req, res) => {
     
 })
-
 router.delete('/:id', (req, res) => {
     
 })
-
 module.exports = router
--- server.js
+++ server.js
@@ -1,41 +1,22 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const axios = require('axios')
+app.use(cors())
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 let port = process.env.PORT || 3000
-
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
 database.on('error', (error) => {
     console.log(error)
 })
-
 database.once('connected', () => {
     console.log('Database Connected');
 })
-
 app.use(express.json());
-
-// Routes
-app.get('/api/tourDates', async (req, res) => {
-  try {
-    const response = await axios.get('https://dttc-api.herokuapp.com/api/tourDates');
-    const tourDates = response.data;
-    res.json(tourDates);
-  } catch (err) {
-    console.error('Error in tour dates request:', err);
-    res.status(500).send('Server error');
-  }
-});
+const tourDatesRouter = require('./routes/tourDates')
+app.use('/tourDates', tourDatesRouter)
 
 const emailRouter = require('./routes/email')
 app.use('/email', emailRouter)
